a6c4c417b7e836f1541a6dc6cd3af8661fefea2c
Jan Piotrowski <piotrowski+github@gmail.com>
2018-08-24 12:40:37 +0200

    hardcode working ruby example from https://github.com/fastlane/fastlane/issues/13122

--- lib/fastlane/plugin/managed_google_play/actions/managed_google_play_action.rb
+++ lib/fastlane/plugin/managed_google_play/actions/managed_google_play_action.rb
@@ -5,7 +5,57 @@ module Fastlane
   module Actions
     class ManagedGooglePlayAction < Action
       def self.run(params)
-        UI.message("The managed_google_play plugin is working!")
+        
+        require "google/apis/playcustomapp_v1"
+
+        # Auth Info
+        @keyfile = ENV['KEYFILE_PATH']
+        @developer_account = ENV['DEVELOPER_ACCOUNT']
+
+        # App Info
+        @apk_path = "artifacts/app-release.apk"
+        @app_title = "App title!"
+        @language_code = "en_US"
+
+        # login
+        scope = 'https://www.googleapis.com/auth/androidpublisher'
+        credentials = JSON.parse(File.open(@keyfile, 'rb').read)
+        auth_client = Signet::OAuth2::Client.new(
+            :token_credential_uri => 'https://oauth2.googleapis.com/token',
+            :audience => 'https://oauth2.googleapis.com/token',
+            :scope => scope,
+            :issuer => credentials['client_id'],
+            :signing_key => OpenSSL::PKey::RSA.new(credentials['private_key'], nil),
+        )
+        UI.message('auth_client: ' + auth_client.inspect)
+        auth_client.fetch_access_token!
+
+        # service
+        play_custom_apps = Google::Apis::PlaycustomappV1::PlaycustomappService.new
+        play_custom_apps.authorization = auth_client
+        UI.message('play_custom_apps with auth: ' + play_custom_apps.inspect)
+
+        # app
+        custom_app = Google::Apis::PlaycustomappV1::CustomApp.new title: @app_title, language_code: @language_code
+        UI.message('custom_app: ' + custom_app.inspect)
+
+        # create app
+        returned = play_custom_apps.create_account_custom_app(
+          @developer_account,
+          custom_app,
+          upload_source: nil #,
+          #upload_source: @apk_path,
+        ) do |created_app, error|
+          unless error.nil?
+            puts "Error: #{error}"
+            UI.error(error.inspect)
+          else
+            puts "Success: #{created_app}."
+            UI.success(created_app)
+          end
+        end
+        UI.message('returned: ' + returned.inspect)
+
       end
 
       def self.description
